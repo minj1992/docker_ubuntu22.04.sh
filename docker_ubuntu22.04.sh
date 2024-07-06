94f018c1a5f09e9960660f2cbbc504d96d6f397a
minj1992 <57328135+minj1992@users.noreply.github.com>
2024-07-06 12:04:49 +0530

    Create docker_ubuntu22.04.sh

---
+++ docker_ubuntu22.04.sh
@@ -0,0 +1,28 @@
+#option to create file --> chmod +x install_docker.sh
+#./install_docker.sh
+
+#!/bin/bash
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key:
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Use the following command to set up the repository:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index again and install Docker:
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify that Docker Engine is installed correctly by running the hello-world image:
+sudo docker run hello-world
